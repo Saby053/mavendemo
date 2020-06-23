65bc5b88a5ae8e2301cbc1acf5645fa5060d067f
Saby053 <57228134+Saby053@users.noreply.github.com>
2020-06-23 12:57:54 +0530

    commit and commit

--- src/main/java/com/Controller.java
+++ src/main/java/com/Controller.java
@@ -27,8 +27,7 @@ public class Controller extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	/**
